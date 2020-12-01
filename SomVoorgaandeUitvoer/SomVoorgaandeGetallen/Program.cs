641322322ff9d91b227c3a558607a22e8a635df1
Thibaut Humblet <thibaut.humblet@telenet.be>
2020-12-01 19:34:52 +0100

    Added som voorgaande uitvoer

---
+++ SomVoorgaandeUitvoer/SomVoorgaandeGetallen/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomVoorgaandeGetallen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // wijzig kleuren console
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+
+            // titelbalk
+            Console.Title = "Som met alle voorgaande getallen";
+
+            
+
+            // afsluiten
+            // Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("\nDruk op ENTER om het programma af te sluiten...");
+
+            // dummy readline
+            Console.ReadLine();
+        }
+    }
+}
+
