ac88476166a09406f91d5301c260bfcef68b9875
Thibaut Humblet <thibaut.humblet@telenet.be>
2020-12-01 20:10:59 +0100

    Added percentagedeelbaarheid

---
+++ PercentagesDeelbaarheid/PercentagesDeelbaarheid/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PercentagesDeelbaarheid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // wijzig kleuren console
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+
+            // titelbalk
+            Console.Title = "Percentages deelbaarheid random gekozen getallen";
+
+           
+
+            // afsluiten
+            // Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("\nDruk op ENTER om het programma af te sluiten...");
+
+            // dummy readline
+            Console.ReadLine();
+        }
+    }
+}
